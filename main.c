eb28da7e1f3c0df04f4241b15908af12429f4a8c
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2025-09-10 11:58:25 -0400

    created variables

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
 int main (){
     // Mad Libs Game
 
+    char noun[50] = "";
+    char verb[50] = "";
+    char adjective1[50] = "";
+    char adjective2[50] = "";
+    char adjective3[50] = "";
 
 
     return 0;
