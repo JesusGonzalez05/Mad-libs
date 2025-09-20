340cca194fa2356321c69960768133411c526014
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2025-09-20 09:49:11 -0400

    created output and input for the user.

--- main.c
+++ main.c
@@ -11,6 +11,16 @@ int main (){
     char adjective2[50] = "";
     char adjective3[50] = "";
 
+    printf("Enter an adjective!\n");
+    fgets(adjective1, sizeof(adjective1), stdin);
+    printf("Enter a noun!\n");
+    fgets(adjective2, sizeof(adjective2), stdin);
+    printf("Enter an adjective!\n");
+    fgets(noun, sizeof(noun), stdin);
+    printf("Enter a verb!\n");
+    fgets(verb, sizeof(verb), stdin);
+    printf("Enter an adjective!\n");
+    fgets(adjective3, sizeof(adjective3), stdin);
 
     return 0;
 }
